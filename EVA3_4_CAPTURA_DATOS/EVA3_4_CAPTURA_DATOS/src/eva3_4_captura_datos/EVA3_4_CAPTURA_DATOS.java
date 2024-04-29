60ad921612c2bfd98aa2ecf6cd08b5f0c2c39491
JazminAndreaBatistaFigueroa <159494335+JazminAndreaBatistaFigueroa@users.noreply.github.com>
2024-04-29 15:53:05 -0700

    Add files via upload

---
+++ EVA3_4_CAPTURA_DATOS/EVA3_4_CAPTURA_DATOS/src/eva3_4_captura_datos/EVA3_4_CAPTURA_DATOS.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_4_captura_datos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jazzb
+ */
+public class EVA3_4_CAPTURA_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cant;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Cuantos nombres vas a capturar");
+        cant = captu.nextInt();
+        String [] listaNombres = new String [cant];
+        captu.nextLine();
+        //Hay que preguntar cada nombre:
+        for (int i = 0; i < listaNombres.length; i++) {
+            System.out.println("Nombre #" + (i + 1) + ":");
+            listaNombres[i] = captu.nextLine();
+            
+        }
+        for (int i = 0; i < listaNombres.length; i++) {
+            System.out.println(listaNombres[i]);
+            
+        
+       
+    }
+        }
+    
+}
